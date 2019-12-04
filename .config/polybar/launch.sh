3242196f9dcf10f82af9404fad9ea7d9c398da5d
rendiwijiatmoko <rendiwijiatmoko97@gmail.com>
2019-12-04 21:08:29 +0700

    update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar with name example
-polybar xavartun -r &
+if type "xrandr"; then
+	  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		      MONITOR=$m polybar -r xavartun &
+		        done
+		else
+			 polybar -r xavartun &
+fi
 
 echo "Bars launched..."
 
